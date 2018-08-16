662b51332eaaa13d503aec5178b6224b2f8e6874
lmckone <lmckone@bu.edu>
2018-08-16 16:54:23 -0400

    adding basic interactivity tests

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import os
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import psycopg2
+import pandas as pd
 
+#connect to the database and read in the necessary tables
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+inflow = pd.read_sql_query("select * from inflow")
 
 app = dash.Dash(__name__)
 server = app.server
@@ -11,11 +16,12 @@ server = app.server
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 app.layout = html.Div([
-    html.H2('Hello World'),
+    html.H2('System Performance Dashboard - Boston CoC'),
+    html.H4('Race')
     dcc.Dropdown(
         id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
+        options=[{'label': i, 'value': i} for i in inflow.race.unique()],
+        value='White'
     ),
     html.Div(id='display-value')
 ])
