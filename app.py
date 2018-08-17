7b564f0009539900bc6397baa91e5e09fa5c4674
lmckone <lmckone@bu.edu>
2018-08-17 11:28:03 -0400

    inflow graph with race filter

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import psycopg2
 import pandas as pd
+import plotly.graph_objs as go
 
 #connect to the database and read in the necessary tables
 DATABASE_URL = os.environ['DATABASE_URL']
@@ -23,13 +24,26 @@ app.layout = html.Div([
         options=[{'label': i, 'value': i} for i in inflow.race.unique()],
         value='White'
     ),
-    html.Div(id='display-value')
+    dcc.Graph(id='inflow-graph')
 ])
 
-@app.callback(dash.dependencies.Output('display-value', 'children'),
+@app.callback(dash.dependencies.Output('inflow-graph', 'figure'),
               [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
+def update_figure(selected_race):
+    filtered_df = inflow[inflow.race == selected_race]
+    print(filtered_df)
+    yearly_inflow = pd.DataFrame(filtered_df.groupby('year')['count'].sum())
+    print(yearly_inflow)
+    return {
+    'data': [go.Bar(x = yearly_inflow.index,
+        y= yearly_inflow['count'])],
+    'layout': go.Layout(
+        title='Yearly Inflow',
+        xaxis={'title': 'Year'},
+        yaxis={'title': 'Number of clients'},
+        hovermode='closest'
+        )
+    }
 
 if __name__ == '__main__':
     app.run_server(debug=True)
